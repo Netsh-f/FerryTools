b879b79688b4f03b768fe5eeeb5eaca67fda7f73
Zhang Wenjin <zhangwenjin@buaa.edu.cn>
2025-11-10 10:52:07 +0800

    重构导航栏，响应式

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -11,30 +11,127 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
-  final List<Widget> _pages = const [
-    HomePage(),
-    SettingPage(),
+
+  static final List<_NavigationItem> _navigationItems = [
+    _NavigationItem(
+      label: 'Home',
+      icon: Icons.home_outlined,
+      selectedIcon: Icons.home,
+      page: const HomePage(),
+    ),
+    _NavigationItem(
+      label: 'Settings',
+      icon: Icons.settings_outlined,
+      selectedIcon: Icons.settings,
+      page: const SettingPage(),
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FerryTools'),
-      ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
+    final bool isWideScreen = MediaQuery.of(context).size.width >= 600;
+    final currentView = _navigationItems[_currentIndex].page;
+
+    if (isWideScreen) {
+      return _DesktopLayout(
         currentIndex: _currentIndex,
-        onTap: (i) => setState(() => _currentIndex = i),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+        currentView: currentView,
+        onDestinationSelected: (index) => setState(() => _currentIndex = index),
+        navigationItems: _navigationItems,
+      );
+    } else {
+      return _MobileLayout(
+        currentIndex: _currentIndex,
+        currentView: currentView,
+        onDestinationSelected: (index) => setState(() => _currentIndex = index),
+        navigationItems: _navigationItems,
+      );
+    }
+  }
+}
+
+class _NavigationItem {
+  const _NavigationItem({
+    required this.label,
+    required this.icon,
+    required this.selectedIcon,
+    required this.page,
+  });
+
+  final String label;
+  final IconData icon;
+  final IconData selectedIcon;
+  final Widget page;
+}
+
+class _MobileLayout extends StatelessWidget {
+  const _MobileLayout({
+    required this.currentIndex,
+    required this.currentView,
+    required this.onDestinationSelected,
+    required this.navigationItems,
+  });
+
+  final int currentIndex;
+  final Widget currentView;
+  final ValueChanged<int> onDestinationSelected;
+  final List<_NavigationItem> navigationItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FerryTools')),
+      body: currentView,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onDestinationSelected,
+        items: navigationItems.map((item) {
+          return BottomNavigationBarItem(
+            icon: Icon(item.icon),
+            activeIcon: Icon(item.selectedIcon),
+            label: item.label,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class _DesktopLayout extends StatelessWidget {
+  const _DesktopLayout({
+    required this.currentIndex,
+    required this.currentView,
+    required this.onDestinationSelected,
+    required this.navigationItems,
+  });
+
+  final int currentIndex;
+  final Widget currentView;
+  final ValueChanged<int> onDestinationSelected;
+  final List<_NavigationItem> navigationItems;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('FerryTools')),
+      body: Row(
+        children: [
+          NavigationRail(
+            minWidth: 72,
+            groupAlignment: -1.0,
+            labelType: NavigationRailLabelType.all,
+            selectedIndex: currentIndex,
+            onDestinationSelected: onDestinationSelected,
+            destinations: navigationItems.map((item) {
+              return NavigationRailDestination(
+                icon: Icon(item.icon),
+                selectedIcon: Icon(item.selectedIcon),
+                label: Text(item.label),
+              );
+            }).toList(),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
+          Expanded(child: currentView),
         ],
       ),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,60 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:ferrytools/widgets/tool_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  static final List<Map<String, dynamic>> _tools = [
-    {'title': '大乐透', 'icon': Icons.code},
-    {'title': '双色球', 'icon': Icons.casino},
-    {'title': '七乐彩', 'icon': Icons.casino},
-    {'title': '七星彩', 'icon': Icons.casino},
-    {'title': '排列三', 'icon': Icons.casino},
-    {'title': '排列五', 'icon': Icons.casino},
-    {'title': '福彩3D', 'icon': Icons.casino},
-    {'title': '快乐8', 'icon': Icons.casino},
-    {'title': '竞彩足球', 'icon': Icons.sports_soccer},
-    {'title': '竞彩足球', 'icon': Icons.sports_soccer},
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 700),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              int crossAxisCount;
-              final width = constraints.maxWidth;
-              if (width < 300) {
-                crossAxisCount = 1;
-              } else if (width < 500) {
-                crossAxisCount = 2;
-              } else {
-                crossAxisCount = 3;
-              }
-
-              return GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: crossAxisCount,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                ),
-                itemCount: _tools.length,
-                itemBuilder: (context, index) {
-                  final tool = _tools[index];
-                  return ToolCard(
-                    title: tool['title'] as String,
-                    icon: tool['icon'] as IconData,
-                  );
-                },
-              );
-            },
-          ),
-        ),
-      ),
+    return const Center(
+      child: Text('Welcome to the Home Page!'),
     );
   }
 }
